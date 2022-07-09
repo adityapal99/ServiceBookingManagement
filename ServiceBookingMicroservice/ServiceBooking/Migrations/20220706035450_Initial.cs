87091819f0564fc03a37f6020a2ded7c6adf1399
chauhan-gaurav <chauhangauravbipin@gmail.com>
2022-07-09 09:01:04 +0530

    BookingMicroservice added

---
+++ ServiceBookingMicroservice/ServiceBooking/Migrations/20220706035450_Initial.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ServiceBooking.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Requests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Productid = table.Column<int>(nullable: false),
+                    Userid = table.Column<int>(nullable: false),
+                    RequestDate = table.Column<DateTime>(nullable: false),
+                    Problem = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Status = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Requests", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ReportDate = table.Column<DateTime>(nullable: false),
+                    ServiceType = table.Column<string>(nullable: true),
+                    ActionTaken = table.Column<string>(nullable: true),
+                    DiagnosisDetails = table.Column<string>(nullable: true),
+                    isPaid = table.Column<string>(nullable: true),
+                    VisitFees = table.Column<int>(nullable: false),
+                    RepairDetails = table.Column<string>(nullable: true),
+                    ServiceRequestId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reports", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reports_Requests_ServiceRequestId",
+                        column: x => x.ServiceRequestId,
+                        principalTable: "Requests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reports_ServiceRequestId",
+                table: "Reports",
+                column: "ServiceRequestId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reports");
+
+            migrationBuilder.DropTable(
+                name: "Requests");
+        }
+    }
+}
---
+++ ServiceBookingMicroservice/ServiceBooking/Models/ServiceReport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ServiceBooking.Models
+{
+    public class ServiceReport
+    {
+        public int Id { get; set; }
+        public DateTime ReportDate { get; set; }
+        public string ServiceType { get; set; }
+        public string ActionTaken { get; set; }
+        public string DiagnosisDetails { get; set; }
+        public string isPaid { get; set; }
+        public int VisitFees { get; set; }
+        public string RepairDetails { get; set; }
+
+        public int ServiceRequestId { get; set; }
+        public ServiceRequest ServiceRequest { get; set; }
+
+    }
+}
