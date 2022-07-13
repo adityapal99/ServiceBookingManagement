e074af6ac1b4aec2be38ad79ffb0caff6014bc08
adityapal99 <adityapal2000.2013@gmail.com>
2022-07-13 18:28:11 +0530

    Auth cors added

--- AuthorizationMicroservice/Startup.cs
+++ AuthorizationMicroservice/Startup.cs
@@ -33,6 +33,11 @@ namespace AuthorizationMicroservice
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "AuthorizationMicroservice", Version = "v1" });
             });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AngularPolicy", policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +53,8 @@ namespace AuthorizationMicroservice
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("AngularPolicy");
 
             app.UseAuthorization();
 
