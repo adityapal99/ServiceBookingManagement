2415b4f372d49e40cb2f70d307920f6047918162
SYNODER <ayushrajvaishlko78438@gmail.com>
2022-07-13 20:55:23 +0530

    Update ServiceRequest.cs

--- ServiceBookingMicroservice/Models/ServiceRequest.cs
+++ ServiceBookingMicroservice/Models/ServiceRequest.cs
@@ -1,16 +1,32 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ServiceBooking.Models
 {
     public class ServiceRequest
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required]
         public int Productid { get; set; }
+
+        [Required]
         public int Userid { get; set; }
+
+        [Required]
         public DateTime RequestDate { get; set; }
+
+        [Required]
         public string Problem { get; set; }
+
+        [Required]
         public string Description { get; set; }
+
+        [Required]
         public string Status { get; set; }
 
         public ICollection<ServiceReport> Reports { get; set; }
