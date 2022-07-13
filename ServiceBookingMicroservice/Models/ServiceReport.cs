6414d39477b33c099c59a3d3db402a73bceb5566
SYNODER <ayushrajvaishlko78438@gmail.com>
2022-07-13 20:54:01 +0530

    Update ServiceReport.cs

--- ServiceBookingMicroservice/Models/ServiceReport.cs
+++ ServiceBookingMicroservice/Models/ServiceReport.cs
@@ -1,18 +1,37 @@
-﻿using System;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ServiceBooking.Models
 {
     public class ServiceReport
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required]
         public DateTime ReportDate { get; set; }
+
+        [Required]
         public string ServiceType { get; set; }
+
+        [Required]
         public string ActionTaken { get; set; }
+
+        [Required]
         public string DiagnosisDetails { get; set; }
+
+        [Required]
         public string isPaid { get; set; }
+
+        [Required]
         public int VisitFees { get; set; }
+
+        [Required]
         public string RepairDetails { get; set; }
 
+        [Required]
         public int ServiceRequestId { get; set; }
         public ServiceRequest ServiceRequest { get; set; }
 
